d206999a2061f85e637b88376f4f339a509258dc
nicoleLab <klaus.kahlfuss@kstp.at>
2022-11-14 21:40:56 +0100

    Initial commit for budget bot: focus on read data from csv via pandas and doing "calculations"

---
+++ budget_telegram_bot.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import datetime as dt
+import pandas as pd
+
+file_name = "YOUR FILE"
+
+def get_monthly_expenses(file):
+    headers = ["Date", "Shop", "Amount"]
+
+    df_budget = pd.read_csv(file, delimiter=",", names=headers)
+    date = dt.date.today()
+    df_budget.month = pd.DatetimeIndex(df_budget["Date"]).month
+    df_budget_result = df_budget[df_budget.month == date.month]
+
+    df_budget_result.year = pd.DatetimeIndex(df_budget_result["Date"]).year
+    df_budget_result = df_budget_result[df_budget_result.year == date.year]
+
+    return df_budget_result
+
+def get_sum_of_expenses(file):
+    monthly_expenses = get_monthly_expenses(file)
+    total_amount_expenses = monthly_expenses.Amount.sum()
+    return total_amount_expenses
+
+def main():
+    result = get_sum_of_expenses(file_name)
+    return result
