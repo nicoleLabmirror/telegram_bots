5190b7c1ec69ba53aa0a64fab867d559c2d51ea1
nicoleLab <klaus.kahlfuss@kstp.at>
2022-11-17 22:02:04 +0100

    List comprehension for monthly expenses per shop Add func main()

--- budget_telegram_bot.py
+++ budget_telegram_bot.py
@@ -5,43 +5,45 @@ import pandas as pd
 
 file_name = "YOUR FILE"
 
+
 def get_monthly_expenses(file):
     headers = ["Date", "Shop", "Amount"]
-
     df_budget = pd.read_csv(file, delimiter=",", names=headers)
-    #date = dt.date.today()
+    date = dt.date.today()
+
     df_budget.month = pd.DatetimeIndex(df_budget["Date"]).month
-    df_budget_result = df_budget[df_budget.month == 4]#date.month]
+    df_budget_result = df_budget[df_budget.month == date.month]
 
     df_budget_result.year = pd.DatetimeIndex(df_budget_result["Date"]).year
-    df_budget_result = df_budget_result[df_budget_result.year == 2021]#date.year]
+    df_budget_result = df_budget_result[df_budget_result.year == date.year]
 
     return df_budget_result
 
-# Callback? Dunno ... still researching :D
+
 def get_sum_of_expenses():
     monthly_expenses = get_monthly_expenses(file_name)
     total_amount_expenses = monthly_expenses.Amount.sum()
+
     return total_amount_expenses
+
 
 def get_expenses_for_each_shop():
     monthly_expenses = get_monthly_expenses(file_name)
-    test_1 = monthly_expenses.groupby(["Shop"]).sum().reset_index()
-    test_2 = []
-    for i in range(0, len(test_1)):
-        x = test_1.Shop[i]
-        y = test_1.Amount[i]
-        test_2.append(
-            [
-                x,
-                y
-            ]
-        )
+    monthly_expenses_groupby = monthly_expenses.groupby(["Shop"]).sum().reset_index()
 
-    return test_2
+    monthly_expenses_per_shop = [
+        [monthly_expenses_groupby.Shop[entry], monthly_expenses_groupby.Amount[entry]]
+        for entry in range(len(monthly_expenses_groupby))
+    ]
+
+    return monthly_expenses_per_shop
 
 
 def main():
-    result = get_sum_of_expenses()
-    test_3 = get_expenses_for_each_shop()
-    return result, test_3
+    total_amount_expenses = get_sum_of_expenses()
+    monthly_expenses_per_shop = get_expenses_for_each_shop()
+    return total_amount_expenses, monthly_expenses_per_shop
+
+
+if __name__ == "__main__":
+    main()
