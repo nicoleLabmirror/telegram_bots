98224883123bec46330566be02430f413efd6483
nicoleLab <klaus.kahlfuss@kstp.at>
2022-11-21 20:48:10 +0100

    Reading data from file as seperate function

--- budget_telegram_bot.py
+++ budget_telegram_bot.py
@@ -4,13 +4,20 @@ import datetime as dt
 import pandas as pd
 
 file_name = "YOUR FILE"
+file_name = "/home/k2/Dokumente/Code/PYTHON/bots/botToado2/toadoHaushalt_K.csv"
+
+date = dt.date.today()
 
 
-def get_monthly_expenses(file):
+def get_data_from_file(file):
     headers = ["Date", "Shop", "Amount"]
     df_budget = pd.read_csv(file, delimiter=",", names=headers)
-    date = dt.date.today()
 
+    return df_budget
+
+
+def get_monthly_expenses():
+    df_budget = get_data_from_file(file_name)
     df_budget.month = pd.DatetimeIndex(df_budget["Date"]).month
     df_budget_result = df_budget[df_budget.month == date.month]
 
@@ -21,14 +28,14 @@ def get_monthly_expenses(file):
 
 
 def get_sum_of_expenses():
-    monthly_expenses = get_monthly_expenses(file_name)
+    monthly_expenses = get_monthly_expenses()
     total_amount_expenses = monthly_expenses.Amount.sum()
 
     return total_amount_expenses
 
 
 def get_expenses_for_each_shop():
-    monthly_expenses = get_monthly_expenses(file_name)
+    monthly_expenses = get_monthly_expenses()
     monthly_expenses_groupby = monthly_expenses.groupby(["Shop"]).sum().reset_index()
 
     monthly_expenses_per_shop = [
