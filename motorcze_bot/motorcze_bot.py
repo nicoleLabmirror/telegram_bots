a69d2f1026bf618bd6bf8f0add9cb4a502aac70e
nicoleLab <klaus.kahlfuss@kstp.at>
2023-05-24 22:57:29 +0200

    Add functions for managing data and import sql helper functions

--- motorcze_bot/motorcze_bot.py
+++ motorcze_bot/motorcze_bot.py
@@ -4,30 +4,48 @@ import telepot as tb
 import telepot.aio.loop
 import telepot.loop
 
+from sql_helper import *
+
 BOT = tb.Bot("BOT_TOKEN")
 CHAT_ID = "CHAT_ID"
 
+BOT = tb.Bot("5922809115:AAHs1god0Lw9l6sd9l-6Gqi2YjzVR8X2Gf8")
+CHAT_ID = 326247323
+
 
 def read_message_from_user(msg):
-    message_text = msg["text"]
-    print("I read messages")
-    handle_message(message_text)
+    user_text = msg["text"]
+    user_id = msg["from"]["id"]
+
+    handle_message(user_text, user_id)
 
 
-def handle_message(message_text):
-    text = message_text.split(" ")
-    text = "test".join(text)
-    print("I change messages")
+def handle_message(user_text, user_id):
+    if user_text == "?":
+        text = read_route_data(user_id)
+    else:
+        text = write_route_data(user_text, user_id)
+
     send_message(text)
 
 
+def read_route_data(user_id):
+    data = read_route_data_from_db(user_id)
+
+    return data
+
+
+def write_route_data(user_text, user_id):
+    user = read_route_data_from_db(user_id)
+    data = write_route_data()
+
+    return data
+
 def send_message(text):
-    print("I send messages")
     BOT.sendMessage(CHAT_ID, text)
 
 
 def main():
-    print("I run the programm")
     telepot.loop.MessageLoop(BOT, read_message_from_user).run_forever()
 
 
