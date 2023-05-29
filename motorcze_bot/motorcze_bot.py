217d3962bd05da406e25fce966ca95310562f8c5
nicoleLab <klaus.kahlfuss@kstp.at>
2023-05-29 07:26:34 +0200

    Import sql helper functions and use them for managing data

--- motorcze_bot/motorcze_bot.py
+++ motorcze_bot/motorcze_bot.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 
+import datetime as dt
+
 import telepot as tb
 import telepot.aio.loop
 import telepot.loop
 
-from sql_helper import *
+from sql_helper import read_route_data_from_db, write_route_data_to_db
 
 BOT = tb.Bot("BOT_TOKEN")
 CHAT_ID = "CHAT_ID"
-
-BOT = tb.Bot("5922809115:AAHs1god0Lw9l6sd9l-6Gqi2YjzVR8X2Gf8")
-CHAT_ID = 326247323
 
 
 def read_message_from_user(msg):
@@ -24,22 +23,37 @@ def handle_message(user_text, user_id):
     if user_text == "?":
         text = read_route_data(user_id)
     else:
-        text = write_route_data(user_text, user_id)
+        write_route_data(user_text, user_id)
+        text = "Route eingetragen"
 
     send_message(text)
 
 
 def read_route_data(user_id):
-    data = read_route_data_from_db(user_id)
+    today_year = dt.date.today().year
+    read_route = {
+        "year": today_year,
+        "person_id": user_id,
+    }
+    data = read_route_data_from_db(read_route)
 
     return data
 
 
 def write_route_data(user_text, user_id):
-    user = read_route_data_from_db(user_id)
-    data = write_route_data()
+    route_date = dt.date.today()
+    route_name = user_text.split(" ")[0]
+    route_distance = float(user_text.split(" ")[1])
+    new_route = {
+        "date": route_date,
+        "name": route_name,
+        "distance": route_distance,
+        "person_id": user_id,
+    }
+    data = write_route_data_to_db(new_route)
 
     return data
+
 
 def send_message(text):
     BOT.sendMessage(CHAT_ID, text)
